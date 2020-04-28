24502e1f5cf687fcb62845f772c0df26a374db1a
Robert Kernan <rkernan@gmail.com>
2020-04-28 05:04:00 -0400

    Fixed kc40 key not working

--- keyboards/dz60/keymaps/rkernan/keymap.c
+++ keyboards/dz60/keymaps/rkernan/keymap.c
@@ -24,7 +24,7 @@
     { k10,   KC_NO, k12,   k13,  k14,   k15,   k16,  k17,   k18,   k19,   k1a,   k1b,  k1c,   k1d,  k1e   }, \
     { k20,   KC_NO, k22,   k23,  k24,   k25,   k26,  k27,   k28,   k29,   k2a,   k2b,  k2c,   k2d,  KC_NO }, \
     { k30,   KC_NO, k32,   k33,  k34,   k35,   k36,  k37,   k38,   k39,   k3a,   k3b,  KC_NO, k3d,  KC_NO }, \
-    { KC_NO, k41,   KC_NO, k43,  KC_NO, KC_NO, k46,  KC_NO, KC_NO, KC_NO, KC_NO, k4b,  KC_NO, k4d,  KC_NO }  \
+    { k40,   k41,   KC_NO, k43,  KC_NO, KC_NO, k46,  KC_NO, KC_NO, KC_NO, KC_NO, k4b,  KC_NO, k4d,  KC_NO }  \
 }
 
 // layers
